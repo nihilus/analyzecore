164462257c3c54d1bc7cc3d891d76e8bebb48737
U-WIN-XP\Admin <nietzsche@lysator.liu.se>
2014-09-21 01:10:25 +0200

    Change back to original Qt4

--- GeneratedFiles/ui_unix_core.h
+++ GeneratedFiles/ui_unix_core.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'unix_core.ui'
 **
-** Created by: Qt User Interface Compiler version 5.3.2
+** Created by: Qt User Interface Compiler version 4.8.6
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,18 +10,18 @@
 #define UI_UNIX_CORE_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QCheckBox>
-#include <QtWidgets/QGridLayout>
-#include <QtWidgets/QHeaderView>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QLineEdit>
-#include <QtWidgets/QPushButton>
-#include <QtWidgets/QSpinBox>
-#include <QtWidgets/QTabWidget>
-#include <QtWidgets/QWidget>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QCheckBox>
+#include <QtGui/QGridLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpinBox>
+#include <QtGui/QTabWidget>
+#include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
 
@@ -62,144 +62,144 @@ public:
     void setupUi(QWidget *core_analysis)
     {
         if (core_analysis->objectName().isEmpty())
-            core_analysis->setObjectName(QStringLiteral("core_analysis"));
+            core_analysis->setObjectName(QString::fromUtf8("core_analysis"));
         core_analysis->resize(403, 227);
         actionConnect = new QAction(core_analysis);
-        actionConnect->setObjectName(QStringLiteral("actionConnect"));
+        actionConnect->setObjectName(QString::fromUtf8("actionConnect"));
         tabs = new QTabWidget(core_analysis);
-        tabs->setObjectName(QStringLiteral("tabs"));
+        tabs->setObjectName(QString::fromUtf8("tabs"));
         tabs->setGeometry(QRect(10, 10, 381, 221));
         login = new QWidget();
-        login->setObjectName(QStringLiteral("login"));
+        login->setObjectName(QString::fromUtf8("login"));
         gridLayoutWidget = new QWidget(login);
-        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
+        gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
         gridLayoutWidget->setGeometry(QRect(0, 0, 381, 191));
         gridLayout = new QGridLayout(gridLayoutWidget);
-        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         gridLayout->setContentsMargins(0, 0, 0, 0);
         label = new QLabel(gridLayoutWidget);
-        label->setObjectName(QStringLiteral("label"));
+        label->setObjectName(QString::fromUtf8("label"));
 
         gridLayout->addWidget(label, 0, 0, 1, 1);
 
         hostname = new QLineEdit(gridLayoutWidget);
-        hostname->setObjectName(QStringLiteral("hostname"));
+        hostname->setObjectName(QString::fromUtf8("hostname"));
 
         gridLayout->addWidget(hostname, 0, 2, 1, 1);
 
         label_3 = new QLabel(gridLayoutWidget);
-        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setObjectName(QString::fromUtf8("label_3"));
 
         gridLayout->addWidget(label_3, 3, 0, 1, 1);
 
         password = new QLineEdit(gridLayoutWidget);
-        password->setObjectName(QStringLiteral("password"));
+        password->setObjectName(QString::fromUtf8("password"));
         password->setEchoMode(QLineEdit::Password);
 
         gridLayout->addWidget(password, 3, 2, 1, 1);
 
         label_4 = new QLabel(gridLayoutWidget);
-        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setObjectName(QString::fromUtf8("label_4"));
 
         gridLayout->addWidget(label_4, 4, 0, 1, 1);
 
         command = new QLineEdit(gridLayoutWidget);
-        command->setObjectName(QStringLiteral("command"));
+        command->setObjectName(QString::fromUtf8("command"));
         command->setEchoMode(QLineEdit::Normal);
 
         gridLayout->addWidget(command, 4, 2, 1, 2);
 
         label_6 = new QLabel(gridLayoutWidget);
-        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setObjectName(QString::fromUtf8("label_6"));
 
         gridLayout->addWidget(label_6, 7, 0, 1, 1);
 
         binary = new QLineEdit(gridLayoutWidget);
-        binary->setObjectName(QStringLiteral("binary"));
+        binary->setObjectName(QString::fromUtf8("binary"));
         binary->setEchoMode(QLineEdit::Normal);
 
         gridLayout->addWidget(binary, 7, 2, 1, 3);
 
         label_2 = new QLabel(gridLayoutWidget);
-        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setObjectName(QString::fromUtf8("label_2"));
 
         gridLayout->addWidget(label_2, 0, 3, 1, 1);
 
         username = new QLineEdit(gridLayoutWidget);
-        username->setObjectName(QStringLiteral("username"));
+        username->setObjectName(QString::fromUtf8("username"));
 
         gridLayout->addWidget(username, 0, 4, 1, 1);
 
         label_5 = new QLabel(gridLayoutWidget);
-        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setObjectName(QString::fromUtf8("label_5"));
 
         gridLayout->addWidget(label_5, 5, 0, 1, 1);
 
         startup = new QLineEdit(gridLayoutWidget);
-        startup->setObjectName(QStringLiteral("startup"));
+        startup->setObjectName(QString::fromUtf8("startup"));
         startup->setEchoMode(QLineEdit::Normal);
 
         gridLayout->addWidget(startup, 5, 2, 1, 2);
 
         save_password = new QCheckBox(gridLayoutWidget);
-        save_password->setObjectName(QStringLiteral("save_password"));
+        save_password->setObjectName(QString::fromUtf8("save_password"));
 
         gridLayout->addWidget(save_password, 3, 3, 1, 1);
 
         checkBox = new QCheckBox(gridLayoutWidget);
-        checkBox->setObjectName(QStringLiteral("checkBox"));
+        checkBox->setObjectName(QString::fromUtf8("checkBox"));
 
         gridLayout->addWidget(checkBox, 5, 4, 1, 1);
 
         connect = new QPushButton(gridLayoutWidget);
-        connect->setObjectName(QStringLiteral("connect"));
+        connect->setObjectName(QString::fromUtf8("connect"));
 
         gridLayout->addWidget(connect, 4, 4, 1, 1);
 
         core = new QLineEdit(gridLayoutWidget);
-        core->setObjectName(QStringLiteral("core"));
+        core->setObjectName(QString::fromUtf8("core"));
         core->setEchoMode(QLineEdit::Normal);
 
         gridLayout->addWidget(core, 8, 2, 1, 3);
 
         label_7 = new QLabel(gridLayoutWidget);
-        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setObjectName(QString::fromUtf8("label_7"));
 
         gridLayout->addWidget(label_7, 8, 0, 1, 1);
 
         tabs->addTab(login, QString());
         debug_control = new QWidget();
-        debug_control->setObjectName(QStringLiteral("debug_control"));
+        debug_control->setObjectName(QString::fromUtf8("debug_control"));
         debug_control->setEnabled(false);
         gridLayoutWidget_2 = new QWidget(debug_control);
-        gridLayoutWidget_2->setObjectName(QStringLiteral("gridLayoutWidget_2"));
+        gridLayoutWidget_2->setObjectName(QString::fromUtf8("gridLayoutWidget_2"));
         gridLayoutWidget_2->setGeometry(QRect(10, 10, 210, 83));
         gridLayout_2 = new QGridLayout(gridLayoutWidget_2);
-        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
         gridLayout_2->setContentsMargins(0, 0, 0, 0);
         stack = new QPushButton(gridLayoutWidget_2);
-        stack->setObjectName(QStringLiteral("stack"));
+        stack->setObjectName(QString::fromUtf8("stack"));
 
         gridLayout_2->addWidget(stack, 2, 1, 1, 2);
 
         sharedlibs = new QPushButton(gridLayoutWidget_2);
-        sharedlibs->setObjectName(QStringLiteral("sharedlibs"));
+        sharedlibs->setObjectName(QString::fromUtf8("sharedlibs"));
 
         gridLayout_2->addWidget(sharedlibs, 1, 1, 1, 2);
 
         framenr = new QSpinBox(gridLayoutWidget_2);
-        framenr->setObjectName(QStringLiteral("framenr"));
+        framenr->setObjectName(QString::fromUtf8("framenr"));
         framenr->setMinimum(1);
 
         gridLayout_2->addWidget(framenr, 3, 2, 1, 1);
 
         registers = new QPushButton(gridLayoutWidget_2);
-        registers->setObjectName(QStringLiteral("registers"));
+        registers->setObjectName(QString::fromUtf8("registers"));
 
         gridLayout_2->addWidget(registers, 3, 3, 1, 1);
 
         curr_frame = new QPushButton(gridLayoutWidget_2);
-        curr_frame->setObjectName(QStringLiteral("curr_frame"));
+        curr_frame->setObjectName(QString::fromUtf8("curr_frame"));
 
         gridLayout_2->addWidget(curr_frame, 3, 1, 1, 1);
 
@@ -224,67 +224,67 @@ public:
 
     void retranslateUi(QWidget *core_analysis)
     {
-        core_analysis->setWindowTitle(QApplication::translate("core_analysis", "Core File Analysis", 0));
-        actionConnect->setText(QApplication::translate("core_analysis", "connect", 0));
+        core_analysis->setWindowTitle(QApplication::translate("core_analysis", "Core File Analysis", 0, QApplication::UnicodeUTF8));
+        actionConnect->setText(QApplication::translate("core_analysis", "connect", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
         tabs->setToolTip(QString());
 #endif // QT_NO_TOOLTIP
-        label->setText(QApplication::translate("core_analysis", "hostname", 0));
+        label->setText(QApplication::translate("core_analysis", "hostname", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        hostname->setToolTip(QApplication::translate("core_analysis", "name of remote unix host to connet to", 0));
+        hostname->setToolTip(QApplication::translate("core_analysis", "name of remote unix host to connet to", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        label_3->setText(QApplication::translate("core_analysis", "password", 0));
+        label_3->setText(QApplication::translate("core_analysis", "password", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        password->setToolTip(QApplication::translate("core_analysis", "remote unix password", 0));
+        password->setToolTip(QApplication::translate("core_analysis", "remote unix password", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        label_4->setText(QApplication::translate("core_analysis", "command", 0));
+        label_4->setText(QApplication::translate("core_analysis", "command", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        command->setToolTip(QApplication::translate("core_analysis", "complete path to debugger", 0));
+        command->setToolTip(QApplication::translate("core_analysis", "complete path to debugger", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        label_6->setText(QApplication::translate("core_analysis", "binary", 0));
+        label_6->setText(QApplication::translate("core_analysis", "binary", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        binary->setToolTip(QApplication::translate("core_analysis", "path to remote binary on host", 0));
+        binary->setToolTip(QApplication::translate("core_analysis", "path to remote binary on host", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
 #ifndef QT_NO_WHATSTHIS
-        binary->setWhatsThis(QApplication::translate("core_analysis", "asdadasd", 0));
+        binary->setWhatsThis(QApplication::translate("core_analysis", "asdadasd", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_WHATSTHIS
-        label_2->setText(QApplication::translate("core_analysis", "username", 0));
+        label_2->setText(QApplication::translate("core_analysis", "username", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        username->setToolTip(QApplication::translate("core_analysis", "username of remote unix host", 0));
+        username->setToolTip(QApplication::translate("core_analysis", "username of remote unix host", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
         username->setText(QString());
-        label_5->setText(QApplication::translate("core_analysis", "startup", 0));
+        label_5->setText(QApplication::translate("core_analysis", "startup", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        save_password->setToolTip(QApplication::translate("core_analysis", "save password in IDB", 0));
+        save_password->setToolTip(QApplication::translate("core_analysis", "save password in IDB", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        save_password->setText(QApplication::translate("core_analysis", "save", 0));
-        checkBox->setText(QApplication::translate("core_analysis", "debug", 0));
+        save_password->setText(QApplication::translate("core_analysis", "save", 0, QApplication::UnicodeUTF8));
+        checkBox->setText(QApplication::translate("core_analysis", "debug", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        connect->setToolTip(QApplication::translate("core_analysis", "connect to/disconnect from remote host", 0));
+        connect->setToolTip(QApplication::translate("core_analysis", "connect to/disconnect from remote host", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        connect->setText(QApplication::translate("core_analysis", "connect", 0));
-        label_7->setText(QApplication::translate("core_analysis", "core path", 0));
-        tabs->setTabText(tabs->indexOf(login), QApplication::translate("core_analysis", "Login", 0));
+        connect->setText(QApplication::translate("core_analysis", "connect", 0, QApplication::UnicodeUTF8));
+        label_7->setText(QApplication::translate("core_analysis", "core path", 0, QApplication::UnicodeUTF8));
+        tabs->setTabText(tabs->indexOf(login), QApplication::translate("core_analysis", "Login", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        stack->setToolTip(QApplication::translate("core_analysis", "Show stack trace", 0));
+        stack->setToolTip(QApplication::translate("core_analysis", "Show stack trace", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        stack->setText(QApplication::translate("core_analysis", "stack trace", 0));
+        stack->setText(QApplication::translate("core_analysis", "stack trace", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        sharedlibs->setToolTip(QApplication::translate("core_analysis", "Display all shared libraries.", 0));
+        sharedlibs->setToolTip(QApplication::translate("core_analysis", "Display all shared libraries.", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        sharedlibs->setText(QApplication::translate("core_analysis", "shared libs", 0));
+        sharedlibs->setText(QApplication::translate("core_analysis", "shared libs", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        framenr->setToolTip(QApplication::translate("core_analysis", "increase or decrease current frame", 0));
+        framenr->setToolTip(QApplication::translate("core_analysis", "increase or decrease current frame", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
 #ifndef QT_NO_TOOLTIP
-        registers->setToolTip(QApplication::translate("core_analysis", "show registers of current frame", 0));
+        registers->setToolTip(QApplication::translate("core_analysis", "show registers of current frame", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        registers->setText(QApplication::translate("core_analysis", "registers", 0));
+        registers->setText(QApplication::translate("core_analysis", "registers", 0, QApplication::UnicodeUTF8));
 #ifndef QT_NO_TOOLTIP
-        curr_frame->setToolTip(QApplication::translate("core_analysis", "show current frame", 0));
+        curr_frame->setToolTip(QApplication::translate("core_analysis", "show current frame", 0, QApplication::UnicodeUTF8));
 #endif // QT_NO_TOOLTIP
-        curr_frame->setText(QApplication::translate("core_analysis", "frame", 0));
-        tabs->setTabText(tabs->indexOf(debug_control), QApplication::translate("core_analysis", "Debugger", 0));
+        curr_frame->setText(QApplication::translate("core_analysis", "frame", 0, QApplication::UnicodeUTF8));
+        tabs->setTabText(tabs->indexOf(debug_control), QApplication::translate("core_analysis", "Debugger", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
--- moc_myactions.cpp
+++ moc_myactions.cpp
@@ -1,53 +1,25 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'myactions.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.6)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include "myactions.h"
-#include <QtCore/qbytearray.h>
-#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'myactions.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.3.2. It"
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.6. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_CoreCallbacks_t {
-    QByteArrayData data[8];
-    char stringdata[101];
-};
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_CoreCallbacks_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData)) \
-    )
-static const qt_meta_stringdata_CoreCallbacks_t qt_meta_stringdata_CoreCallbacks = {
-    {
-QT_MOC_LITERAL(0, 0, 13),
-QT_MOC_LITERAL(1, 14, 13),
-QT_MOC_LITERAL(2, 28, 0),
-QT_MOC_LITERAL(3, 29, 9),
-QT_MOC_LITERAL(4, 39, 13),
-QT_MOC_LITERAL(5, 53, 15),
-QT_MOC_LITERAL(6, 69, 16),
-QT_MOC_LITERAL(7, 86, 14)
-    },
-    "CoreCallbacks\0connect_rexec\0\0dbg_stack\0"
-    "dbg_registers\0dbg_shared_libs\0"
-    "dbg_frame_select\0dbg_curr_frame"
-};
-#undef QT_MOC_LITERAL
-
 static const uint qt_meta_data_CoreCallbacks[] = {
 
  // content:
-       7,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
        6,   14, // methods
@@ -57,28 +29,28 @@ static const uint qt_meta_data_CoreCallbacks[] = {
        0,       // flags
        0,       // signalCount
 
- // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    0,   47,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
-
- // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
+ // slots: signature, parameters, type, tag, flags
+      15,   14,   14,   14, 0x08,
+      31,   14,   14,   14, 0x08,
+      43,   14,   14,   14, 0x08,
+      59,   14,   14,   14, 0x08,
+      77,   14,   14,   14, 0x08,
+      96,   14,   14,   14, 0x08,
 
        0        // eod
+};
+
+static const char qt_meta_stringdata_CoreCallbacks[] = {
+    "CoreCallbacks\0\0connect_rexec()\0"
+    "dbg_stack()\0dbg_registers()\0"
+    "dbg_shared_libs()\0dbg_frame_select()\0"
+    "dbg_curr_frame()\0"
 };
 
 void CoreCallbacks::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
         CoreCallbacks *_t = static_cast<CoreCallbacks *>(_o);
         switch (_id) {
         case 0: _t->connect_rexec(); break;
@@ -93,21 +65,28 @@ void CoreCallbacks::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
     Q_UNUSED(_a);
 }
 
-const QMetaObject CoreCallbacks::staticMetaObject = {
-    { &QObject::staticMetaObject, qt_meta_stringdata_CoreCallbacks.data,
-      qt_meta_data_CoreCallbacks,  qt_static_metacall, 0, 0}
+const QMetaObjectExtraData CoreCallbacks::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
 };
 
+const QMetaObject CoreCallbacks::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_CoreCallbacks,
+      qt_meta_data_CoreCallbacks, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &CoreCallbacks::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
 
 const QMetaObject *CoreCallbacks::metaObject() const
 {
-    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *CoreCallbacks::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_CoreCallbacks.stringdata))
+    if (!strcmp(_clname, qt_meta_stringdata_CoreCallbacks))
         return static_cast<void*>(const_cast< CoreCallbacks*>(this));
     return QObject::qt_metacast(_clname);
 }
@@ -120,10 +99,6 @@ int CoreCallbacks::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
-            *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 6;
     }
     return _id;
